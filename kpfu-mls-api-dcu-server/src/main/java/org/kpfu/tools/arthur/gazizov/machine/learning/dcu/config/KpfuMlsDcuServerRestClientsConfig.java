79b2562600f0468d3f7ded2498ccdb7521359b41
arthurgazizov <artynoobs@gmail.com>
2017-11-25 02:05:09 +0300

    ML-19 DCU: use engine inference client & enable this in config

--- kpfu-mls-api-dcu-server/src/main/java/org/kpfu/tools/arthur/gazizov/machine/learning/dcu/config/KpfuMlsDcuServerRestClientsConfig.java
+++ kpfu-mls-api-dcu-server/src/main/java/org/kpfu/tools/arthur/gazizov/machine/learning/dcu/config/KpfuMlsDcuServerRestClientsConfig.java
@@ -1,5 +1,7 @@
 package org.kpfu.tools.arthur.gazizov.machine.learning.dcu.config;
 
+import org.kpfu.tools.arthur.gazizov.machine.learning.engine.inference.rest.client.EngineInferenceClient;
+import org.kpfu.tools.arthur.gazizov.machine.learning.engine.inference.rest.client.EngineInferenceClientImpl;
 import org.kpfu.tools.arthur.gazizov.machine.learning.fs.client.FsClient;
 import org.kpfu.tools.arthur.gazizov.machine.learning.fs.client.FsClientImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,16 @@ public class KpfuMlsDcuServerRestClientsConfig {
   private KpfuMlsDcuServerConfigurationProperties properties;
 
   @Bean
-  public FsClient fsClient(){
+  public FsClient fsClient() {
     return FsClientImpl.Builder.aFsClientImpl()
             .serverUrl(properties.getMlsFsServerUrl())
+            .build();
+  }
+
+  @Bean
+  public EngineInferenceClient engineInferenceClient() {
+    return EngineInferenceClientImpl.Builder.aFsClientImpl()
+            .serverUrl(properties.getMlsEngineInferenceServerUrl())
             .build();
   }
 }
--- kpfu-mls-api-dcu-server/src/main/java/org/kpfu/tools/arthur/gazizov/machine/learning/dcu/service/inference/InferenceServiceGatewayImpl.java
+++ kpfu-mls-api-dcu-server/src/main/java/org/kpfu/tools/arthur/gazizov/machine/learning/dcu/service/inference/InferenceServiceGatewayImpl.java
@@ -1,8 +1,12 @@
 package org.kpfu.tools.arthur.gazizov.machine.learning.dcu.service.inference;
 
 import org.kpfu.tools.arthur.gazizov.machine.learning.dcu.dto.inference.InferenceDto;
+import org.kpfu.tools.arthur.gazizov.machine.learning.engine.inference.rest.client.EngineInferenceClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author Arthur Gazizov (Cinarra Systems)
@@ -10,9 +14,18 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service("inferenceServiceGatewayImpl")
 public class InferenceServiceGatewayImpl implements InferenceService {
+  @Autowired
+  private EngineInferenceClient engineInferenceClient;
+
   @Override
   public InferenceDto makeInference(Long dataSetId, MultipartFile multipartFile) {
-    // TODO: 25/11/2017 use kpfu-mls-inference-client
-    return null;
+    byte[] bytes = null;
+    try {
+      // TODO: 25/11/2017 check that bytes[] == all bytes in file
+      bytes = multipartFile.getBytes();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return engineInferenceClient.makeInference(dataSetId, bytes);
   }
 }
